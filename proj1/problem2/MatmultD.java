fccba46acf7fe2bb7edb6d7793a933894e156743
falconlee236 <falconlee236@cau.ac.kr>
2024-04-04 15:44:34 +0900

    change code

--- proj1/problem2/MatmultD.java
+++ proj1/problem2/MatmultD.java
@@ -2,46 +2,32 @@ package proj1.problem2;
 
 import java.util.Scanner;
 
-// command-line execution example) java MatmultD 6 < mat500.txt
-// 6 means the number of threads to use
-// < mat500.txt means the file that contains two matrices is given as standard input
-//
-// In eclipse, set the argument value and file input by using the menu [Run]->[Run Configurations]->{[Arguments], [Common->Input File]}.
-
-// Original JAVA source code: http://stackoverflow.com/questions/21547462/how-to-multiply-2-dimensional-arrays-matrix-multiplication
-public class MatmultD {
-    private static Scanner sc = new Scanner(System.in);
-    public static void main(String [] args)
-    {
+public class MatmultD { //125231132
+    private static final int THREAD_NUM = 1;
+    private static final Scanner sc = new Scanner(System.in);
+    public static void main(String [] args) {
         int thread_no=0;
         if (args.length==1) thread_no = Integer.valueOf(args[0]);
-        else thread_no = 1;
+        else thread_no = THREAD_NUM;
 
         int a[][]=readMatrix();
         int b[][]=readMatrix();
 
+        MatmultDMatrixResult result_matrix = new MatmultDMatrixResult(a.length, b[0].length);
         long startTime = System.currentTimeMillis();
-        MatmultD_Matrix_Result result_matrix = new MatmultD_Matrix_Result(a.length, b[0].length);
+        MatmultDThread[] threads = new MatmultDThread[thread_no];
 
-        // Thread creation
-        MatmultD_Thread[] threads = new MatmultD_Thread[thread_no];
-        for (int i=0; i<thread_no; i++) {
-            threads[i] = new MatmultD_Thread(i, thread_no, a, b, result_matrix);
+        for (int i = 0; i < thread_no; i++) {
+            threads[i] = new MatmultDThread(i, thread_no, a, b, result_matrix);
             threads[i].start();
         }
-        for (int i=0; i<thread_no; i++) {
-            try {
-                threads[i].join();
-            } catch (InterruptedException ignored) {
-                System.out.println("Thread joining failed.");
-            }
+
+        for (int i = 0; i < thread_no; i++) {
+            try {threads[i].join();}
+            catch (InterruptedException ignored) {}
         }
         long endTime = System.currentTimeMillis();
-
-        //printMatrix(a);
-        //printMatrix(b);
         printMatrix(result_matrix.matrix);
-
         System.out.printf("[thread_no]:%2d , [Time]:%4d ms\n", thread_no, endTime-startTime);
     }
 
@@ -74,34 +60,27 @@ public class MatmultD {
     }
 }
 
-class MatmultD_Matrix_Result {
+class MatmultDMatrixResult {
     int[][] matrix;
-
-    public MatmultD_Matrix_Result(int row, int column) {
+    public MatmultDMatrixResult(int row, int column) {
         this.matrix = new int[row][column];
     }
-
-    synchronized void set_result(int row, int column, int result) {
+    synchronized void setResult(int row, int column, int result) {
         matrix[row][column] = result;
     }
 }
 
-class MatmultD_Thread extends Thread {
-    final int thread, num_of_threads;
+class MatmultDThread extends Thread {
+    final int thread_id, thread_num;
     final int a[][], b[][];
-    MatmultD_Matrix_Result result_matrix;
-    public MatmultD_Thread(
-            int thread,
-            int num_of_threads,
-            int a[][],
-            int b[][],
-            MatmultD_Matrix_Result result_matrix
-    ) {
-        this.thread = thread;
-        this.num_of_threads = num_of_threads;
+    MatmultDMatrixResult res_mat;
+    public MatmultDThread(int thread_id, int thread_num, int a[][], int b[][],
+                           MatmultDMatrixResult res_mat) {
+        this.thread_id = thread_id;
+        this.thread_num = thread_num;
         this.a = a;
         this.b = b;
-        this.result_matrix = result_matrix;
+        this.res_mat = res_mat;
     }
 
     @Override
@@ -111,18 +90,18 @@ class MatmultD_Thread extends Thread {
         int m = a.length;
         int p = b[0].length;
 
-        for (int id_to_multiply = thread; id_to_multiply < m * p; id_to_multiply += num_of_threads) {
-            int col = id_to_multiply % m;
-            int row = (id_to_multiply - col) / m;
+        for (int i = thread_id; i < m * p; i += thread_num) {
+            int col = i % m;
+            int row = (i - col) / m;
 
-            int result = 0;
-            for(int k=0; k<n; k++) {
-                result += a[row][k] * b[k][col];
+            int res = 0;
+            for(int k = 0; k < n; k++) {
+                res += a[row][k] * b[k][col];
             }
 
-            result_matrix.set_result(row, col, result);
+            res_mat.setResult(row, col, res);
         }
         long endTime = System.currentTimeMillis();
-        System.out.printf("thread_no: %d\nCalculation Time: %d ms\n", thread, endTime-startTime);
+        System.out.printf("thread_no: %d\nCalculation Time: %d ms\n", thread_id, endTime-startTime);
     }
 }
