2477665fe83cd97edeb937d4499623f66070a4d1
falconlee236 <falconlee236@cau.ac.kr>
2024-04-30 20:14:24 +0900

    finish ex3

--- proj2/prob3/ex3.java
+++ proj2/prob3/ex3.java
@@ -1,4 +1,28 @@
 package proj2.prob3;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ex3 {
+    public static void main(String[] args) {
+        AtomicInteger counter = new AtomicInteger(0);
+
+        for(int i = 0; i < 5; i++){
+            int id = i;
+            new Thread(()->{
+                try{
+                    while (true){
+                        System.out.printf("Thread %d: is get %d from counter\n", id, counter.get());
+                        Thread.sleep(1000);
+                        counter.set(counter.get() + 1);
+                        System.out.printf("Thread %d: is set %d to counter\n", id, counter.get());
+                        Thread.sleep(1000);
+                        System.out.printf("Thread %d: is called getAndAdd(1), return = %d\n", id, counter.getAndAdd(1));
+                        Thread.sleep(1000);
+                        System.out.printf("Thread %d: is called addAndGet(1) return = %d\n", id, counter.addAndGet(1));
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException ignored) {}
+            }).start();
+        }
+    }
 }
